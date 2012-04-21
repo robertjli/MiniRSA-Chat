4765de49998a5e01502fe5d88a154a3d0f89e058
Robert Li <li.robertj+github@gmail.com>
2012-04-20 23:04:55 -0400

    Reader and writer message exchange

--- MiniRSA Chat/src/chat/ChatClient.java
+++ MiniRSA Chat/src/chat/ChatClient.java
@@ -132,6 +132,8 @@ public class ChatClient {
         Thread writer = new Thread(
                             new WriterThread(socket, partnerExp, partnerMod));
         writer.start();
+        
+        
     }
     
     /**
--- MiniRSA Chat/src/chat/ReaderThread.java
+++ MiniRSA Chat/src/chat/ReaderThread.java
@@ -2,10 +2,7 @@ package chat;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.net.Socket;
-import java.util.ArrayList;
-
 import minirsa.MiniRSA;
 
 /**
@@ -41,29 +38,12 @@ public class ReaderThread implements Runnable {
             in = new BufferedReader(
                     new InputStreamReader(
                             socket.getInputStream()));
-            String line;
             while (true) {
-                line = in.readLine();    
-                System.err.println("Reading line " + line); //debug
-                String inputString = line;
-                System.out.println("Received encrypted message: " + inputString);
+                String line = in.readLine();    
+                System.out.println("> " + line);
                 
-                // Break into longs of length <= 5
-                ArrayList<Long> longs = stringToLongs(inputString);
-                
-                // Encrypt each long
-                ArrayList<Long> encryptedArray = new ArrayList<Long>();
-                for (Long decrypted : longs) {
-                    Long encrypted = Long.valueOf(MiniRSA.endecrypt(decrypted.longValue(), exponent, modulus));
-                    encryptedArray.add(encrypted);
-                }
-                
-                // convert longs to text
-                String encryptedText = longsToString(encryptedArray);
-                System.out.println("Send: " + encryptedText);
-                
-                // write input to socket
-                out.println(encryptedText);
+                String[] chars = line.split(" ");
+                System.out.println(">> " + decrypt(chars));
             }  
         } catch (Exception e) {
             System.err.println("IO error in Reader");
@@ -72,40 +52,15 @@ public class ReaderThread implements Runnable {
         }
     }
     
-    private String convertToAscii(String string) {
-        StringBuilder asciiString = new StringBuilder("");
-        for (int i = 0; i < string.length(); i++) {
-            Integer ascii = (int) string.charAt(i);
-            String s = ascii.toString();
-            // if ascii value is 2-digits, append 0
-            if (s.length() == 2) asciiString.append(0);
-            asciiString.append(s);
+    private String decrypt(String[] chars) {
+        String ret = "";
+        for (String s : chars) {
+            long decrypt = MiniRSA.endecrypt(
+                                Integer.parseInt(s), exponent, modulus);
+            int ascii = (int) (decrypt % 128); // 128 chars in ASCII
+            ret += (char) ascii;
         }
-        return asciiString.toString();
-    }
-    
-    private String longsToString(ArrayList<Long> longs) {
-        StringBuilder textString = new StringBuilder("");
-        for (Long l : longs) {
-            String c = l.toString();
-            textString.append(c);
-        }
-        return textString.toString();
-    }
-    
-    private ArrayList<Long> stringToLongs(String ascii) {
-        ArrayList<Long> longs = new ArrayList<Long>();
-        for (int i = 0; i < ascii.length(); i++) {
-            String piece;
-            try {
-                piece = ascii.substring(i, i + 5);
-            } catch (Exception e) {
-                piece = ascii.substring(i);
-            }
-            Long l = Long.valueOf(piece);
-            longs.add(l);
-        }
-        return longs;
+        return ret;
     }
 
 }
